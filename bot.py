a834183db9e39c96a0c3c54fca60e4a7a3f24cff
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2022-02-06 17:34:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ Bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
-db = Database()
 
 START_TEXT = """ `Hai {}, 
 Am a YouTube Downloader Bot I Can Download Songs,Videos and Lyrics From YouTube and lyrics and  Would upload into Telegram. 
@@ -111,9 +110,7 @@ async def cb_handler(bot, update):
         
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    if not await db.is_user_exist(update.from_user.id):
-        await db.add_user(update.from_user.id)  
-
+   
     await update.reply_text(
         text=START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
@@ -127,34 +124,6 @@ async def about(bot, update):
         disable_web_page_preview=True,
         reply_markup=ABOUT_BUTTONS
     )
-@Bot.on_message(filters.private & filters.command("status"), group=5)
-async def status(bot, update):
-    total_users = await db.total_users_count()
-    text = "**Music Bot Status**\n"
-    text += f"\n**Total Users hit start:** `{total_users}`"
-    await update.reply_text(
-        text=text,
-        quote=True,
-        disable_web_page_preview=True
-    )
-
-broadcast_ids = {}
-
-async def send_msg(user_id, message):
-    try:
-        await message.copy(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
 @Bot.on_message(filters.command(["song"]) & ~filters.edited)
@@ -387,90 +356,6 @@ async def inline(client: Client, query: InlineQuery):
                 switch_pm_parameter="",
             )
         
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.reply)
-async def broadcast_(c, m):
-    print("broadcasting......")
-    if m.from_user.id not in Config.OWNER_ID:
-        await c.delete_messages(
-            chat_id=m.chat.id,
-            message_ids=m.message_id,
-            revoke=True
-        )
-        return
-    all_users = await db.get_all_users()
-    broadcast_msg = m.reply_to_message
-    
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    
-    out = await m.reply_text(
-        text = f"Broadcast initiated! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    
-    broadcast_ids[broadcast_id] = dict(
-        total = total_users,
-        current = done,
-        failed = failed,
-        success = success
-    )
-    
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            
-            sts, msg = await send_msg(
-                user_id = int(user['id']),
-                message = broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            
-            if sts == 400:
-                await db.delete_user(user['id'])
-            
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current = done,
-                        failed = failed,
-                        success = success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-    
-    await asyncio.sleep(3)
-    
-    await out.delete()
-    
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    
-    await aiofiles.os.remove('broadcast.txt')
 
 @Bot.on_message(filters.command("lyrics"))
 async def lrsearch(_, message: Message):  
